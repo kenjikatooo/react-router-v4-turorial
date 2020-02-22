7341dfce6c9e19fa95e0eb77369341363209b379
kenjikatooo <k.524.10nis@gmail.com>
2020-02-22 17:50:34 +0900

    set link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { BrowserRouter, Route, Link } from 'react-router-dom'
 const App = () => (
   <BrowserRouter>
     <div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/friends'>Friends</Link></li>
+      </ul>
+      <hr />
       <Route exact path='/' component={Home} />
       <Route path='/about' component={About} />
       <Route path='/friends' component={Friends} />
